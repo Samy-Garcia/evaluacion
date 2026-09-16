02f5b7dcf317499230035ce52fe232e2cba248bd
Maur1CG <20240746@ricaldone.edu.sv>
2026-09-16 08:08:16 -0600

    arreglar errores

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,21 +1,28 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import { API_KEY, AUTH_DOMAIN, PROJECT_ID, STORAGE_BUCKET, MESSAGING_SENDER_ID, APP_ID } from "@env";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDGa__Z3y125momPYGI4E0a02zM5gVrkAA",
-  authDomain: "evaluacion-6317b.firebaseapp.com",
-  projectId: "evaluacion-6317b",
-  storageBucket: "evaluacion-6317b.firebasestorage.app",
-  messagingSenderId: "453155714045",
-  appId: "1:453155714045:web:e8f6736c06f280a63a9f94",
-  measurementId: "G-2MLWK2KD5J"
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID
 };
+
+console.log("Valor de congiguración de Firebase:", firebaseConfig);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+const database = getFirestore(app);
+
+const storage = getStorage(app);
+
+export { database, storage };
